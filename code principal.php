e7c62a8921584aac182f8bb0b8e5ccddb90d3c36
noetendo <noetendo84@gmail.com>
2022-10-02 10:57:44 +0200

    update code principal

--- code principal.php
+++ code principal.php
@@ -5,11 +5,61 @@
         <meta charset="utf-8" />
     </head>
     <body>
-        <h2>Affichage de texte avec PHP</h2>
-        
-        <p>
-            Cette ligne a été écrite entièrement en HTML.<br />
-            <?php echo("Celle-ci a été écrite entièrement en PHP."); ?>
-        </p>
-    </body>
-</html>
+    
+            <p>on est le <?php echo date('d/m/Y '); ?></p>
+
+            <?php
+$isEnabled = true; // La condition d'accès
+
+if ($isEnabled == true) {
+    echo "Vous êtes autorisé(e) à accéder au site ✅";}?>
+    </br>
+    
+    <?php
+
+$grade = 10;
+
+switch ($grade) 
+{ 
+    case 0: // dans le cas où $grade vaut 0
+        echo "Tu es vraiment un gros nul !!!";
+    break;
+    
+    case 5: // dans le cas où $grade vaut 5
+        echo "Tu es très mauvais";
+    break;
+    
+    case 7: // dans le cas où $grade vaut 7
+        echo "Tu es mauvais";
+    break;
+    
+    case 10: // etc. etc.
+        echo "Tu as pile poil la moyenne, c'est un peu juste…";
+    break;
+    
+    case 12:
+        echo "Tu es assez bon";
+    break;
+    
+    case 16:
+        echo "Tu te débrouilles très bien !";
+    break;
+    
+    case 20:
+        echo "Excellent travail, c'est parfait !";
+    break;
+    
+    default:
+        echo "Désolé, je n'ai pas de message à afficher pour cette note";
+}
+?>
+<?php
+$lines = 1;
+
+while ($lines <= 100) {
+    echo 'Je ne dois pas regarder les mouches voler quand j\'apprends le PHP.<br />';
+    $lines++; // $lines = $lines + 1
+}
+?>
+</body>
+  </html>
